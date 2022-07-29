eb0d82e1f8a98923edd2fbaa38e30a34ddc6d74c
Naimad <59909116+Naimadx123@users.noreply.github.com>
2022-07-29 21:34:45 +0200

    v1.0.0

--- exit.js
+++
@@ -1,8 +0,0 @@
-const { m1, m2 } = require(`../utils/children.js`);
-
-module.exports = {
-  name: 'exit',
-  execute(code) {
-    console.log('Process exit event with code: ', code);
-  }
-};
--- unhandledRejection.js
+++
@@ -1,8 +0,0 @@
-const { m1, m2 } = require(`../utils/children.js`);
-
-module.exports = {
-  name: 'unhandledRejection',
-  execute(code) {
-    console.log('Unhandled Rejection at:', promise, 'reason:', reason);
-  }
-};
